3377447886b393048cd114dc293bb27448b7ad72
Panshyn Ivan <vanya.pantera2517@gmail.com>
2023-03-20 22:58:38 +0300

    Added Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -5,24 +5,23 @@
 // [1,0,1,1,0,1,0,0]
 
 int[] array = new int[8];
-Random rand = new Random();
 
-Array(array);
+FillArray(array);
 WriteArray(array);
 
-void Array(int[] arr)
+void FillArray(int[] arr)
 {
-Random rand = new Random();
-for (int i = 0; i < arr.Length; i++)
-{
-  arr[i] = rand.Next(0, 2);
-}
+  Random rand = new Random();
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = rand.Next(0, 2);
+  }
 }
 
 void WriteArray(int[] arr)
 {
- for (int i = 0; i < arr.Length; i++)
- {
-  Console.Write(arr[i]);
- }
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write(arr[i] + "  ");
+  }
 }
