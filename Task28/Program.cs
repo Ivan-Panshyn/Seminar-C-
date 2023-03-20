e9512517b0cb7763a663523c2a61528e6b5401d9
Panshyn Ivan <vanya.pantera2517@gmail.com>
2023-03-20 22:12:34 +0300

    Added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, 
+// 1. которая  принимает на вход число N и 
+// 2. выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Ввелите натуральное число: ");
+int number =  Convert.ToInt32(Console.ReadLine());
+
+if (number > 0) // если значение верно, что if > 0 выполняется
+{
+  int factNumber = FactNumber(number);
+  Console.WriteLine($"Произведение чисел от 1 до {number} = {factNumber}");
+}
+else // или если значение не верно и if < 0 
+{
+  Console.WriteLine($"Введите натуральное число");
+}
+
+
+int FactNumber (int num)
+{
+  int fact = 1;
+  for (int i = 1; i <= num; i++)
+  {
+    checked // проверка перепонения, когда программа выдает ошибку
+    {
+      fact *= i;
+    }
+  }
+  return fact;
+}
